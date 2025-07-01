0c4c6dc3b6686155fcb5ee9e381c71a855e19324
ratimics <ratimics@ratimics.com>
2025-07-01 01:07:22 -0700

    feat: Add comprehensive tests for mesh upload functionality, including isolated, mock, and simplified scenarios

--- src/system/physics.h
+++ src/system/physics.h
@@ -1,8 +1,8 @@
 #ifndef SYSTEM_PHYSICS_H
 #define SYSTEM_PHYSICS_H
 
-#include "core.h"
-#include "systems.h"
+#include "../core.h"
+#include "../render.h"
 
 void physics_system_update(struct World* world, RenderConfig* render_config, float delta_time);
 
